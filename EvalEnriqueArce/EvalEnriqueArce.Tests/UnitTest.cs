07e61494a7b3618a08fd188df2a296616e6c20ca
ENRIQUE ARCE ALEJANDRO <a2019112108@estudiantes.upsa.edu.bo>
2020-09-18 22:43:47 -0400

    UnitTest Creado

--- EvalEnriqueArce/EvalEnriqueArce.Tests/UnitTest.cs
+++ EvalEnriqueArce/EvalEnriqueArce.Tests/UnitTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Web.Http;
 using System.Web.Http.Results;
+using DocumentFormat.OpenXml.Wordprocessing;
+using EvalEnriqueArce.Controllers;
 using EvalEnriqueArce.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -13,25 +15,25 @@ namespace EvalEnriqueArce.Tests
         public void TestGet()
         {
             //arrange
-            /*PaisesController controller = new PaisesController();
+            PaisesController controller = new PaisesController();
             Pais pais = new Pais();
             {
                 Name = "Bolivia",
                 Capital = "Sucre",
                 Poblacion = 1501211,
-                LatLng = 150°,85°,
-                TimeZone = 9:04/15/19,
+                LatLng = "-150°,85°",
+                TimeZone = "9:04/15/19",
                 Moneda = "Euros",
                 Bandera = "https://www.pinterest.com/marcela7340/bolivia/"
             };
             //act
             IHttpActionResult ActionResult = controller.PostPais(pais);
             var createdResult = ActionResult as
-            CreatedAtRouteNegotiatedContentResult<Paises>;
+            CreatedAtRouteNegotiatedContentResult<PaisesController>;
             //assert
             Assert.IsNotNull(createdResult);
             Assert.AreEqual("DefaultApi", createdResult.RouteName);
-            Assert.IsNotNull(createdResult.RouteValues["name"]);*/
+            Assert.IsNotNull(createdResult.RouteValues["name"]);
         }
     }
 }
